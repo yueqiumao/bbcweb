2a86fbff3dde26902b1f7c2b4de9954d14ca073e
wameidemao <u2nn@qq.com>
2019-10-22 17:00:45 +0800

    update

--- bbc/httpconnection.py
+++ bbc/httpconnection.py
@@ -7,15 +7,16 @@ class HttpConnection:
     async def setup(self):
         print("http connection setup ...")
 
+        self.app = aiohttp.web.Application()
+        self.app.add_routes([
+            aiohttp.web.get("/", self.index)
+        ])
+
     async def run(self):
         print("http connection run ...")
 
+        loop = asyncio.get_event_loop()
+        self.srv = await loop.create_server(self.app.make_handler(), "0.0.0.0", 8000)
 
-# async def index(request):
-#     return aiohttp.web.Response(text=u"这里是大球的小破车，大球的小破车将来可是要上天的")
-
-# app = aiohttp.web.Application()
-# app.add_routes([
-#     aiohttp.web.get("/", index)
-# ])
-# aiohttp.web.run_app(app)
+    async def index(self, request):
+        return aiohttp.web.Response(text=u"这里是大球的小破车，大球的小破车将来可是要上天的")
